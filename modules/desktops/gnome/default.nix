8ac4fc72d89eba949450f4d9e3549d9d57f44af8
anthonymolinari <amolinari017@gmail.com>
2025-06-18 22:31:01 -0700

    move desktops to modules

---
+++ modules/desktops/gnome/default.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }: {
+    services.xserver = {
+        enable = true;
+        displayManager.gdm.enable = true;
+        desktopManager.gnome.enable = true;
+    };
+
+    environment.systemPackages = with pkgs; [
+        gnomeExtensions.blur-my-shell
+        gnomeExtensions.just-perfection
+        gnomeExtensions.arc-menu
+    ];
+
+}
