7cce0838f4c2eb2050347bc129d4c5b244296513
anthonymolinari <amolinari017@gmail.com>
2025-06-23 18:10:20 -0700

    add theme to yazi config

--- modules/apps/yazi/default.nix
+++ modules/apps/yazi/default.nix
@@ -1,12 +1,28 @@
-{ pkgs, ... }: {
-
+{ lib, pkgs, ... }: {
 
     home.packages = with pkgs; [
         yazi
+        bat
     ];
+
 
     programs.yazi = {
         enable = true;
+        settings = {
+            manager = {
+                sort_by = "natural";
+                sort_sensitive = true;
+                sort_reverse = false;
+                sort_dir_first = true;
+                show_hidden = true;
+                show_symlink = true;
+                linemode = "none";
+            };
+            keymap = {
+
+            };
+            theme = lib.importTOML ./catppuccin-mocha-teal.toml;
+        };
     };
 
 }
