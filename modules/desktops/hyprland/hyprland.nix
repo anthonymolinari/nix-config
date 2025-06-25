81cbfb2b3ffdcfc53a811cc893feebc33209ba66
anthonymolinari <amolinari017@gmail.com>
2025-06-25 15:09:40 -0700

    update hyprland config

--- modules/desktops/hyprland/hyprland.nix
+++ modules/desktops/hyprland/hyprland.nix
@@ -10,9 +10,18 @@
     wayland.windowManager.hyprland = {
         enable = true;
         settings = {
-            monitor = "eDP-1, 1920x1080@60, 0x0, 1.0";
+            monitor = ",preferred,0x0,1.0";
+            "$terminal" = "kitty";
+            "$fileManager" = "dolphin";
+            "$menu" = "wofi --show drun";
             "$mod" = "SUPER";
-            exec-once="waybar"; # start status bar
+            exec-once= [
+                " waybar" # start status bar
+            ];
+            windowrule = [
+                "suppressevent maximize, class:.*"
+                "nofocus,class:^$,title:^$,xwayland:1,fullscreen:0,pinned:0"
+            ];
             bindel = [
                 ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
                 ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
@@ -25,7 +34,6 @@
                 "$mod, RETURN, exec, kitty"
                 "SUPER SHIFT, Q, killactive,"
                 "SUPER SHIFT, E, exit,"
-                "SUPER SHIFT, S, exec, hyprlock"
                 "$mod, F, fullscreen,"
                 "$mod, E, exec, dolphon"
                 "$mod, G, exec, firefox"
@@ -38,6 +46,8 @@
                 "$mod, J, movefocus, d"
                 "$mod, K, movefocus, u"
                 "$mod, L, movefocus, r"
+                "$mod, S, togglespecialworkspace, magic"
+                "$SUPER SHIFT, S, movetoworkspace, special:magic"
             ]
             ++ (
                 builtins.concatLists (builtins.genList (i:
@@ -49,6 +59,53 @@
                 )
                 9)
             );
+
+            general = {
+                gaps_in = 2.5;
+                gaps_out = 5;
+                border_size = 1;
+                resize_on_border = false;
+                allow_tearing = false;
+                layout = "dwindle";
+            };
+
+            decoration = {
+                rounding = 0;
+                rounding_power = 0;
+
+                active_opacity = 1.0;
+                inactive_opacity = 1.0;
+
+                shadow = {
+                    enabled = true;
+                    range = 4;
+                    render_power = 3;
+                    color = "rgba(1a1a1aee)";
+                };
+
+                blur = {
+                    enabled = true;
+                    size = 3;
+                    passes = 1;
+                    vibrancy = 0.1696;
+                };
+            };
+
+            dwindle = {
+                pseudotile = true;
+                preserve_split = true;
+            };
+
+            master = {
+                new_status = "master";
+            };
+
+            misc = {};
+            input = {};
+            gestures = {
+                workspace_swipe = true;
+            };
+            device = {};
         };
     };
 
@@ -56,10 +113,10 @@
         enable = true;
         settings = {
             preload = [
-                "~/Pictures/wallpapers/wallpaper_1.jpg"
+                "~/Pictures/wallpapers/wallpaper_2.jpg"
             ];
             wallpaper = [
-                "eDP-1, ~/Pictures/wallpapers/wallpaper_1.jpg"
+                "eDP-1, ~/Pictures/wallpapers/wallpaper_2.jpg"
             ];
         };
     };
